c9773ad93c6e1226d14d48bcc5bd7601fa8135a4
William Gomes <william.desenv@gmail.com>
2026-02-05 20:05:07 -0400

    atualizando projeto

---
+++ src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
@@ -0,0 +1,30 @@
+package com.algaworks.algashop.ordering.presentation;
+
+
+import com.algaworks.algashop.ordering.application.customer.management.CustomerInput;
+import com.algaworks.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerManagementApplicationService customerManagementApplicationService;
+    private final CustomerQueryService customerQueryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerOutput create(@RequestBody CustomerInput input) {
+        UUID customerId = customerManagementApplicationService.create(input);
+        return customerQueryService.findById(customerId);
+    }
+
+}
