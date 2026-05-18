065b29fb1d99216bf3b156ae37864fa49655bee6
William Gomes <william.desenv@gmail.com>
2026-05-18 10:41:41 -0400

    Criando abstracao para os testes com testes containers adaptados

--- src/test/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationServiceIT.java
@@ -1,5 +1,6 @@
 package com.algaworks.algashop.ordering.application.customer.management;
 
+import com.algaworks.algashop.ordering.application.AbstractApplicationIT;
 import com.algaworks.algashop.ordering.application.customer.notification.CustomerNotificationApplicationService;
 import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
 import com.algaworks.algashop.ordering.application.customer.query.CustomerQueryService;
@@ -9,49 +10,18 @@ import com.algaworks.algashop.ordering.domain.model.customer.CustomerNotFoundExc
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerRegisteredEvent;
 import com.algaworks.algashop.ordering.infrastructure.listener.customer.CustomerEventListener;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
-import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.PostgreSQLContainer;
+
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-@SpringBootTest
-@Transactional
-class CustomerManagementApplicationServiceIT {
-
-    private static PostgreSQLContainer postgreSQLContainer
-            = new PostgreSQLContainer<>("postgres:17-alpine")
-            .withDatabaseName("ordering_test");
-
-    @BeforeAll
-    public static void beforeAll() {
-        System.setProperty("api.version", "1.44");
-        postgreSQLContainer.start();
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        postgreSQLContainer.stop();
-    }
-
-    @DynamicPropertySource
-    public static void configurePropertySource(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-        registry.add("spring.flyway.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.flyway.user", postgreSQLContainer::getUsername);
-        registry.add("spring.flyway.password", postgreSQLContainer::getPassword);
-    }
+class CustomerManagementApplicationServiceIT extends AbstractApplicationIT {
 
     @Autowired
     private CustomerManagementApplicationService customerManagementApplicationService;
--- src/test/java/com/algaworks/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
@@ -1,5 +1,6 @@
 package com.algaworks.algashop.ordering.application.customer.query;
 
+import com.algaworks.algashop.ordering.application.AbstractApplicationIT;
 import com.algaworks.algashop.ordering.domain.model.commons.Email;
 import com.algaworks.algashop.ordering.domain.model.commons.FullName;
 import com.algaworks.algashop.ordering.domain.model.customer.Customer;
@@ -15,11 +16,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-// @Sql(scripts = "classpath:sql/clean-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
-// @Sql(scripts = "classpath:sql/clean-database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-@Transactional
-class CustomerQueryServiceIT {
+
+class CustomerQueryServiceIT extends AbstractApplicationIT {
     @Autowired
     private CustomerQueryService queryService;
 
